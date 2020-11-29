aa651485ea94553b76f237f2496cb133bb7139c4
usercspa <spacchbk@gmail.com>
2020-11-29 18:48:33 +0000

    json

--- main.py
+++ main.py
@@ -8,6 +8,77 @@ from fastai.metrics import error_rate
 import warnings
 warnings.filterwarnings('ignore')
 
-help(untar_data)
-path = untar_data(URLs.PETS)
-print(path)
+import eazyml
+
+
+username = 'process.env.username'
+
+password = 'process.env.password'
+
+train_file_path = '/VMS/home/raj/eazyml/train.csv'
+
+
+# To authenticate and retrieve an authentication token to be used for
+
+# all API calls
+
+resp = eazyml.ez_auth(username, password)
+
+auth_token = resp["token"]
+
+
+options = {
+
+    "id": "null",
+
+    "impute": "yes",
+
+    "outlier": "no",
+
+    "discard": ["Column_Names"],
+
+    "accelerate": "yes",
+
+    "outcome" : "Outcome_Column"
+
+}
+
+
+# To upload training data in a file. The accepted file formats are CSV
+
+# and Microsoft Excel.
+
+resp = eazyml.ez_load(auth_token, train_file_path, options)
+
+dataset_id = resp["dataset_id"]
+
+
+options = {
+
+    "outcome_type" : "categorical"
+
+}
+
+
+# To specify the outcome variable and optionally it's data type.
+
+outcome_set = eazyml.ez_set_outcome(auth_token, dataset_id,
+
+              "charges", options)
+
+
+#Resp: {
+
+#'status_code': 200,
+
+#u'message': u'Outcome has been set successfully!',
+
+#u'dataset_id': u'7457',
+
+#u'success': True
+
+#}
+
+
+
+
